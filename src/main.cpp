e19da6797f6e97db01ec0ab83c60ab8967dbd750
HazemMeqdad <hazemmeqdad@gmail.com>
2023-04-23 02:11:36 +0300

    Fix delay timer

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,26 @@
 
 #define bazzerd 11
 
+const int th_min = 1800000000; // 30m
+const int fi_min = 300000000; // 5m
+
 void setup() {
   pinMode(bazzerd, OUTPUT);
+
   Serial.begin(9600);
 }
-void notice();
+void notice(int count = 3);
 
 void loop() {
-  delay(1000 * 60 * 30);  // 30m
+  notice(1);
+  delay(th_min);  // 30m
   notice();
-  delay(1000 * 60 * 5); // 5m
+  delay(fi_min); // 5m
   notice();
 }
 
-void notice() {
-    for (int i = 0;i<3;i++){
+void notice(int count = 3) {
+    for (int i = 0;i<count;i++){
       Serial.println("ring");
       digitalWrite(bazzerd, HIGH);
       delay(400);
